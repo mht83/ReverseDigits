b83000208f65eac776ca5870bf03c73b417f90ba
Mohammadhassan Tohidi <tohidihassan9@gmail.com>
2024-03-26 14:38:00 +0330

    Add project files.

---
+++ ReverseDigits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ReverseDigits
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer to reverse its digits: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = Reverse(number);
+        Console.WriteLine($"The reversed number is: {reversedNumber}");
+    }
+
+    static int Reverse(int number)
+    {
+        int reversed = 0;
+        while (number != 0)
+        {
+            reversed = (reversed * 10) + (number % 10);
+            number /= 10;
+        }
+        return reversed;
+    }
+}
